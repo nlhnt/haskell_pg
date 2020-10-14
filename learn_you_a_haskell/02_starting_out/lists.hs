6d4231984645706913442f8259c87bbd1548a784
mborawski <marcin.borawski@pg.edu.pl>
2020-10-14 16:38:59 +0200

    not so epic and not hilarious

--- learn_you_a_haskell/02_starting_out/lists.hs
+++ learn_you_a_haskell/02_starting_out/lists.hs
@@ -7,3 +7,14 @@ naturalNumbers = [x*2 | x <- [1..10], x*2 >= 12]
     -- else we replace it with a "BANG!"
     -- only if number is less than 10
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+-- several predicates
+fewPredicatesFunct = [ x | x <- [10..20], x /= 13, x /= 15, x /= 19]
+
+-- get products of all available combinations of products of x and y elements
+allCombinations = [ x*y | x <- [2,5,10], y <- [8,10,11]]
+
+-- as above, but products must evaluate to be more than 50
+allMoreThan50 = [ x*y | x <- [2,5,10], y <- [8,10,11], x*y > 50]
+
+-- finished at adjectives and nouns lists
